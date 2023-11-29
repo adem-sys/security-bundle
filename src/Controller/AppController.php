f0927f3ab1256ed24917e7d48d4d4fb9e972d97b
adem-sys <57320153+adem-sys@users.noreply.github.com>
2023-11-30 00:38:31 +0100

    project_auth

---
+++ src/Controller/AppController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+class AppController extends AbstractController
+{
+    #[Route('/homepage',name:'app_homepage')]
+    public function index(){
+        return $this->render('app/index.html.twig',[
+            'user'=> $this->getUser()
+        ]);
+    }
+
+
+    #[Route('/not-the-homepage',name:'alternate_page')]
+    public function alternatePage(){
+        return $this->render('app/alternate-page.html.twig',[
+            'user'=> $this->getUser()
+        ]);
+    }
+}
